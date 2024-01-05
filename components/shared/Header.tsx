e579b1ffb095d212f890d5e0f9aa037ce581f6b4
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2024-01-05 14:10:02 +0530

    Update logo size in Header component

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,14 +1,13 @@
 import Image from "next/image"
 import Link from "next/link"
-import { HiMenuAlt3 } from "react-icons/hi";
 import Menu from "./Menu";
 import NavItems from "./NavItems";
 const Header = () => {
     return (
         <header className="w-full border-b">
             <div className="wrapper flex items-center justify-between">
-                <Link href='/' className="w-36 ">
-                    <Image src='/assets/images/logo.svg' width={300} height={20} alt="logo"></Image>
+                <Link href='/'>
+                    <Image src='/assets/images/logo.svg' width={250} height={20} alt="logo"></Image>
                 </Link>
                 <div className="flex items-center justify-end">
 
