f32cba7e7e87d6ad7e45da3f4169ba3b6e9a36b2
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2024-01-25 19:15:32 +0530

    Remove commented out code in GalleryComponent

--- components/shared/GalleryComponent.tsx
+++ components/shared/GalleryComponent.tsx
@@ -1,12 +1,6 @@
 
 import getFileNames from '@/utils/getFileNames';
 import ImageComponent from './ImageComponent';
-// import LightGallery from 'lightgallery/react';
-// import 'lightgallery/css/lightgallery.css';
-// import 'lightgallery/css/lg-zoom.css';
-// import 'lightgallery/css/lg-thumbnail.css';
-// import lgThumbnail from 'lightgallery/plugins/thumbnail';
-// import lgZoom from 'lightgallery/plugins/zoom';
 const GalleryComponent = async () => {
 
     const onInit = () => {
@@ -18,14 +12,7 @@ const GalleryComponent = async () => {
         <div className='flex flex-col items-center justify-between flex-wrap gap-2 lg:flex-row'>
             {
                 fileNames.map((fileName) => (
-                    // <LightGallery
-                    //     onInit={onInit}
-                    //     speed={500}
-                    //     plugins={[lgThumbnail, lgZoom]}
-                    // >
                     <ImageComponent fileName={fileName} />
-                    // </LightGallery>
-
                 ))
             }
         </div >
