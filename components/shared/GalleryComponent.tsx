e0f3075d2f57c6adbc5d63beb22be4a8ca99027e
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2024-01-26 10:05:41 +0530

    Remove unused code and font files

--- components/shared/GalleryComponent.tsx
+++ components/shared/GalleryComponent.tsx
@@ -2,12 +2,8 @@
 import getFileNames from '@/utils/getFileNames';
 import ImageComponent from './ImageComponent';
 const GalleryComponent = async () => {
-
-    const onInit = () => {
-        console.log('lightGallery has been initialized');
-    };
     const fileNames = await getFileNames('assets/images/gallery')
-    console.log(fileNames)
+    // console.log(fileNames)
     return (
         <div className='flex flex-col items-center justify-between flex-wrap gap-2 lg:flex-row'>
             {
